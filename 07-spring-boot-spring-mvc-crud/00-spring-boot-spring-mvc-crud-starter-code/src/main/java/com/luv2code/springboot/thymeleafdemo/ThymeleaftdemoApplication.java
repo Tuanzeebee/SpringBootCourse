0d59139fefc6c6d7aaa3ff72ae07ae00cd5fd0fe
Tuanzeebee <tuanzeebee@gmail.com>
2025-01-15 21:57:20 +0700

    mvc crud how to add update delete for thymeleaf init

---
+++ 07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/thymeleafdemo/ThymeleaftdemoApplication.java
@@ -0,0 +1,13 @@
+package com.luv2code.springboot.thymeleafdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleaftdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleaftdemoApplication.class, args);
+	}
+
+}
